4273383341afc6c8470d2fbea5a759ac65ac7ab5
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-06-27 11:11:30 +0300

    handel

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -43,7 +43,7 @@ func main() {
 func handleName(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hello, %v!", vars["PARAM"])))	
+	fmt.Fprintf(w, "Hello, %v!", vars["PARAM"])	
 }
 
 func handleBad(w http.ResponseWriter, r *http.Request) {
